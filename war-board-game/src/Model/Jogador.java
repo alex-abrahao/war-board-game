130e297b2a10a81172de82d6847af5aa4369264a
Fernanda Castro <fefeacastro@hotmail.com>
2020-05-10 17:24:20 -0300

    :sparkles: adding model classes

---
+++ war-board-game/src/Model/Jogador.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Jogador {
+	
+	private String name;
+	private String color;
+	private int orderToPlay;
+	private Carta []cards;
+	private Territorio []territorios;
+	
+	
+	
+	
+	
+}
