00e0d5a7fddf59e0ee8afb099f549e8c22e66950
Fernanda Castro <fefeacastro@hotmail.com>
2020-05-12 15:16:03 -0300

    :sparkles: seting players objectives

--- war-board-game/src/Model/Objective.java
+++ war-board-game/src/Model/Objective.java
@@ -1,5 +1,6 @@
 package Model;
 import java.util.Random;
+import java.util.Arrays; 
 
 public class Objective {
     final String description;
@@ -18,10 +19,21 @@ public class Objective {
         return type;
     }
 
-    public Objective getRandomObjective(Objective[] objectives){
+    public int getRandomIndexObjective(Objective[] objectives){
         Random generator = new Random();
 		int randomIndex = generator.nextInt(objectives.length);
-		return objectives[randomIndex];
+		return randomIndex;
 
+    }
+
+    public void setPlayerObjective(Player[] players, Objective[] objectives){
+        for(int i=0; i<players.length; i++){
+            int index = getRandomIndexObjective(objectives)
+            players[i].setObjective(objectives[index]);
+            //removing the selected objective from the list so anyone take the same one.
+            for(int j = index; j < objectives.length -1; j++){
+                objectives[j] = objectives[j + 1]; 
+              }
+        }
     }
 }
