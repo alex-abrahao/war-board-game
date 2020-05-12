c25dde7628b7e64708f07dd6b298f1765e30c0f6
Alexandre Abrahão <alexandre1997pf@hotmail.com>
2020-05-12 10:04:49 -0300

    :sparkles: Adds objective type

--- war-board-game/src/Model/Objective.java
+++ war-board-game/src/Model/Objective.java
@@ -2,8 +2,10 @@ package Model;
 
 public class Objective {
     final String description;
+    final ObjectiveType type;
 
-    Objective(String description) {
+    Objective(String description, ObjectiveType type) {
         this.description = description;
+        this.type = type;
     }
 }
---
+++ war-board-game/src/Model/ObjectiveType.java
@@ -0,0 +1,5 @@
+package Model;
+
+public enum ObjectiveType {
+    numberOfTerritories, defeatPlayer, conquerContinents;
+}
