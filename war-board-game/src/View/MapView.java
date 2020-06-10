509c439b3a2d5da86d15d0e63c1bd3d6734a24a9
Alexandre Abrahão <alexandre1997pf@hotmail.com>
2020-06-09 23:04:13 -0300

    :sparkles: Adds next round and play dice buttons

--- war-board-game/src/View/MapView.java
+++ war-board-game/src/View/MapView.java
@@ -3,6 +3,7 @@ package View;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -13,19 +14,50 @@ public class MapView extends JPanel {
 	private static final int DEFAULT_HEIGHT = 785;
     private BufferedImage mapImage;
     private BufferedImage backgroundImage;
+    private JButton throwDiceButton = new JButton(new ImageIcon(getClass().getResource("/images/war_btnJogarDados.png")));
+    private JButton nextRoundButton = new JButton(new ImageIcon(getClass().getResource("/images/war_btnProxJogada.png")));
 
     public MapView() {
         try {
             mapImage = ImageIO.read(getClass().getResource("/images/war_tabuleiro_mapa copy.png"));
             backgroundImage = ImageIO.read(getClass().getResource("/images/war_tabuleiro_fundo.png"));
-        }
-        catch(IOException e) {
+        } catch(IOException e) {
             System.out.print("Erro ao carregar imagem" + e.getMessage());
         }
+        setLayout(null);
+        setupButtons();
     }
 
-    public BufferedImage getMapImage() {
-        return mapImage;
+    private void setupButtons() {
+        throwDiceButton.setBounds(15, DEFAULT_HEIGHT - 115, 65, 65);
+        throwDiceButton.setBorder(BorderFactory.createEmptyBorder());
+        nextRoundButton.setBounds(90, DEFAULT_HEIGHT - 115, 65, 65);
+        nextRoundButton.setBorder(BorderFactory.createEmptyBorder());
+        
+        throwDiceButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                didPressThrowDice();
+            }
+        });
+
+        nextRoundButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                didPressNextRound();
+            }
+        });
+
+        add(throwDiceButton);
+        add(nextRoundButton);
+    }
+
+    private void didPressNextRound() {
+        System.out.println("Nova rodada");
+    }
+
+    private void didPressThrowDice() {
+        System.out.println("Jogar dados");
     }
 
     @Override
@@ -41,7 +73,6 @@ public class MapView extends JPanel {
 		Dimension screenSize = tk.getScreenSize();
 		int x = screenSize.width/2 - DEFAULT_WIDTH/2;
         int y = screenSize.height/2 - DEFAULT_HEIGHT/2;
-        
         frame.setBounds(x, y, DEFAULT_WIDTH, DEFAULT_HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
