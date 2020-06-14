74753da30b254450950cc051ef6e1b5cf04a0140
Fernanda Castro <fefeacastro@hotmail.com>
2020-06-14 15:13:19 -0300

    ✨ new way to add players

--- war-board-game/src/View/PlayerOptionsView.java
+++ war-board-game/src/View/PlayerOptionsView.java
@@ -9,7 +9,6 @@ public class PlayerOptionsView extends JFrame {
     private static final int DEFAULT_HEIGHT = 300;
 
     private JButton doneButton;
-    private ButtonGroup colorGroup = new ButtonGroup();
 
     public PlayerOptionsView() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,50 +16,31 @@ public class PlayerOptionsView extends JFrame {
         setResizable(false);
         getContentPane().setLayout(null);
 
+        setTitle();
         createTextField();
+        setButton();
     }
 
     private void createTextField() {
-        
-        JTextField nameTextField = new JTextField("nome do jogador");
-        nameTextField.setBounds(20, 40, 220, 30);
-        JLabel title = new JLabel("Digite o nome do jogador:");
-        title.setBounds(20, 8, 220, 40);
-
-        doneButton = new JButton("Pronto");
-        doneButton.setBounds(20, 190, 120, 40);
-
-        add(title);
-        add(nameTextField);
-        add(doneButton);
-        colorOptions(new String[] {
-            "vermelho",
-            "verde",
-            "azul",
-            "amarelo",
-            "preto",
-            "branco"
-        });
-    }
-
-    private void colorOptions(String[] colorNames) {
-        JLabel title = new JLabel("Escolha a cor do jogador:");
-        title.setBounds(20, 60, 220, 30);
-        add(title);
-
-        for (int i = 0; i < colorNames.length; i++) {
-            final int xPosition = (i % 2 == 0) ? 20 : 120;
-            final int yPosition = 80 + 30 * ( (i % 2 == 0) ? i/2 : (i-1)/2 );
-            JRadioButton colorButton = newColorRadioButton(colorNames[i], xPosition, yPosition);
-            add(colorButton);
-            colorGroup.add(colorButton);
+        JTextField[] nameTextField = new JTextField[6];
+        for (int i = 0; i < 6; i++) {
+            nameTextField[i] = new JTextField("nome do jogador " + (i+1));
+            final int yPosition = 40 + 30 * i;
+            nameTextField[i].setBounds(20, yPosition, 220, 30);
+            add(nameTextField[i]);
         }
     }
 
-    private JRadioButton newColorRadioButton(String colorName, int x, int y) {
-        JRadioButton colorButton = new JRadioButton(colorName);
-        colorButton.setBounds(x, y, 90, 30);
-        return colorButton;
+    private void setButton(){
+        doneButton = new JButton("Pronto");
+        doneButton.setBounds(20, 220, 120, 40);
+        add(doneButton);
+    }
+
+    private void setTitle(){
+        JLabel title = new JLabel("Digite o nome dos jogadores:");
+        title.setBounds(20, 8, 220, 40);
+        add(title);
     }
 
     public static void main(String[] args) {
