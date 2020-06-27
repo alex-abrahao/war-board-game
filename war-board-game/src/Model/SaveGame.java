0210e88959b3a2060d7973b5ab9093ac73e938d9
Fernanda Castro <fefeacastro@hotmail.com>
2020-06-27 19:00:49 -0300

    ✨starting save

---
+++ war-board-game/src/Model/SaveGame.java
@@ -0,0 +1,65 @@
+package Model;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SaveGame {
+    public static void saveBinario(Object []objecLists, String fileName) {
+      File file = new File(fileName);
+      try {
+        file.delete();
+        file.createNewFile();
+      
+        ObjectOutputStream objOutput = new ObjectOutputStream(new FileOutputStream(file));
+        objOutput.writeObject(objecLists);
+        objOutput.close();
+      
+      } catch(IOException erro) {
+          System.out.printf("Erro: %s", erro.getMessage());
+      }
+    }
+
+    // public static ArrayList<Object> readBinario(String fileName) {
+    //   ArrayList<Object> lista = new ArrayList();
+    //   try {
+    //     File arq = new File(fileName);
+    //     if (arq.exists()) {
+    //        ObjectInputStream objInput = new ObjectInputStream(new FileInputStream(arq));
+    //        lista = (ArrayList<Object>)objInput.readObject();
+    //        objInput.close();
+    //     }
+    //   } catch(IOException erro1) {
+    //       System.out.printf("Erro: %s", erro1.getMessage());
+    //   } catch(ClassNotFoundException erro2) {
+    //       System.out.printf("Erro: %s", erro2.getMessage());
+    //   }
+    
+    //   return(lista);
+    // }
+
+    public static void readBinario(String fileName) {
+      Object[] lista = new Object[2];
+      try {
+        File arq = new File(fileName);
+        if (arq.exists()) {
+           ObjectInputStream objInput = new ObjectInputStream(new FileInputStream(arq));
+           lista = (Object[])objInput.readObject();
+           objInput.close();
+        }
+      } catch(IOException erro1) {
+          System.out.printf("Erro: %s", erro1.getMessage());
+      } catch(ClassNotFoundException erro2) {
+          System.out.printf("Erro: %s", erro2.getMessage());
+      }
+      System.out.println(lista);
+      //return(lista);
+    }
+
+
+    
+}
